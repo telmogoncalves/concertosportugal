95fa3344f32b6ec9ba32ca2917cb8df5cdbbdbda
Telmo Goncalves <telmo@hey.com>
2024-06-06 10:30:30 +0100

    Commit message

--- src/lib/components/homepage/featured.svelte
+++ src/lib/components/homepage/featured.svelte
@@ -37,7 +37,7 @@ export let concert: Concert & { venue: Venue; artists: Artist[]; dates: string[]
           <a
             href="/artists?see={artist.slug}"
             data-sveltekit-preload-data="hover"
-            class="flex items-center space-x-3 whitespace-nowrap rounded-xl px-4 py-2 hover:bg-secondary md:justify-center"
+            class="flex items-center space-x-3 whitespace-nowrap rounded-xl px-4 py-2 ring-primary hover:ring-2 md:justify-center"
           >
             <img src={artist.image} alt={artist.name} class="h-12 w-12 rounded-full object-cover" />
             <div>
